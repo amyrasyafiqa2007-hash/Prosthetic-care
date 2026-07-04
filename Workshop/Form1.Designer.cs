bb8b28fdcab38c6824fd23dc0195face74eea834
JOVIANNE CHAN YIENG CHI <jovianne_25013937@utp.edu.my>
2026-07-04 14:53:10 +0800

    xixix

--- Workshop/Form1.Designer.cs
+++ Workshop/Form1.Designer.cs
@@ -29,23 +29,36 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            button1 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(236, 169);
+            label1.Location = new Point(295, 211);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(52, 20);
+            label1.Size = new Size(63, 25);
             label1.TabIndex = 0;
             label1.Text = "HELLO";
             // 
+            // button1
+            // 
+            button1.Location = new Point(586, 126);
+            button1.Name = "button1";
+            button1.Size = new Size(115, 45);
+            button1.TabIndex = 1;
+            button1.Text = "Click Me";
+            button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1000, 562);
+            Controls.Add(button1);
             Controls.Add(label1);
+            Margin = new Padding(4, 4, 4, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -55,5 +68,6 @@
         #endregion
 
         private Label label1;
+        private Button button1;
     }
 }
